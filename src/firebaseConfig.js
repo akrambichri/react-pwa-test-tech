2e24c5bf0671f32c5afbfa003f514e84eaddccac
BICHRI Akram <47328724+akrambichri@users.noreply.github.com>
2022-05-21 06:40:21 +0200

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getStorage } from "firebase/storage";
 
 // Initialize Firebase
 const app = initializeApp({
-  apiKey: "AIzaSyCD2l-_khwYqfcOQskaXpgiirVd6C7Pidg",
-  authDomain: "delta-process-test.firebaseapp.com",
-  projectId: "delta-process-test",
-  storageBucket: "delta-process-test.appspot.com",
-  messagingSenderId: "772620139255",
-  appId: "1:772620139255:web:b14dc698a928cef11731f2",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 });
 
 // Firebase storage reference
